8ba73314cc9a4816e0cff5047a9bce71f1ac3cfb
lgy881228 <510687394@qq.com>
2020-07-22 15:22:43 +0800

    init

---
+++ XEngineApp/XEngineApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XEngineApp
+//
+//  Created by edz on 2020/7/22.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
